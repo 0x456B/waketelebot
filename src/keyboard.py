4facd728aaa54c90700ee31d0b166e8d9b6665c1
0x456b <ek324@ya.ru>
2021-05-05 22:53:12 +0300

    add emoji to menu

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,12 +1,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
-button_sign_workout = KeyboardButton('Записаться на тренировку')
-button_accommodation = KeyboardButton('Забронировать проживание')
-button_balance_on_subscription = KeyboardButton('Узнать остаток сетов на абонементе')
-button_instagram = KeyboardButton('Instagram')
-button_public_vk = KeyboardButton('Группа Вконтакте')
-button_call = KeyboardButton('Позвонить')
+button_sign_workout = KeyboardButton('🏄 Записаться на тренировку')
+button_accommodation = KeyboardButton('🏘 Забронировать проживание')
+button_balance_on_subscription = KeyboardButton('❓ Узнать остаток сетов на абонементе')
+button_instagram = KeyboardButton('📸 Instagram')
+button_public_vk = KeyboardButton('🐶 Группа Вконтакте')
+button_call = KeyboardButton('📞 Позвонить')
 
 button_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button_sign_workout)\
     .add(button_accommodation).add(button_balance_on_subscription)\
--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import logging
 import gspread
 import keyboard as kb
 
+
 import aiogram.utils.markdown as md
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -40,23 +41,28 @@ class Form(StatesGroup):
     number_subscriptions = State()
 
 
-@dp.message_handler(text='Записаться на тренировку')
+@dp.message_handler(text='🏄 Записаться на тренировку')
 async def sign_workout(message: types.Message):
     await message.answer('https://widget.bookform.ru/2822/view/58189DA6855C11EA8420E412A7274106/',
                          reply_markup=kb.button_markup)
 
 
-@dp.message_handler(text='Забронировать проживание')
+@dp.message_handler(text='🏘 Забронировать проживание')
 async def accommodation(message: types.Message):
     await message.answer('https://widget.bookform.ru/30366', reply_markup=kb.button_markup)
 
 
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
-    await message.reply("Добро пожаловать!", reply_markup=kb.button_markup)
+    await message.answer("Привет\n\nЯ бот Вейк Дачи\n" +
+                        "Через меня ты можешь:\n\n" +
+                        "🏄 Записаться на тренировку\n" +
+                        "🏘 Забронировать проживание\n" +
+                        "❓ Узнать остаток твоих сетов на абоне\n" +
+                        "👍 И другие полезные функции", reply_markup=kb.button_markup)
 
 
-@dp.message_handler(text='Узнать остаток сетов на абонементе')
+@dp.message_handler(text='❓ Узнать остаток сетов на абонементе')
 async def cmd_start(message: types.Message):
     await Form.number_subscriptions.set()
     await message.reply("Введите номер абонемента:", reply_markup=kb.button_markup)
@@ -83,17 +89,17 @@ async def get_number_subscription(message: types.Message, state: FSMContext):
             )
 
 
-@dp.message_handler(text='Instagram')
+@dp.message_handler(text='📸 Instagram')
 async def get_to_instagram(message: types.Message):
     await message.reply('https://vk.com/wakedacha', reply_markup=kb.button_markup)
 
 
-@dp.message_handler(text='Группа Вконтакте')
+@dp.message_handler(text='🐶 Группа Вконтакте')
 async def get_to_public_vk(message: types.Message):
     await message.answer('https://www.instagram.com/wakedacha', reply_markup=kb.button_markup)
 
 
-@dp.message_handler(text='Позвонить')
+@dp.message_handler(text='📞 Позвонить')
 async def call_to_admin(message: types.Message):
     await message.answer('Номер администратора: \n +79214464498', reply_markup=kb.button_markup)
 
