26064d1dbe2883ad62d694d7ed21cda4376df20f
0x456b <ek324@ya.ru>
2021-05-05 20:35:53 +0300

    add keyboard and replace commands with text message

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import logging
 import gspread
+import keyboard as kb
 
 import aiogram.utils.markdown as md
 from aiogram import Bot, Dispatcher, types
@@ -30,14 +31,30 @@ storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 logging.basicConfig(level=logging.INFO)
 
+
 class Form(StatesGroup):
     number_subscriptions = State()
 
 
+@dp.message_handler(text='Записаться на тренировку')
+async def sign_workout(message: types.Message):
+    pass
+
+
+@dp.message_handler(text='Забронировать проживание')
+async def accommodation(message: types.Message):
+    pass
+
+
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
+    await message.reply("Добро пожаловать!", reply_markup=kb.button_markup)
+
+
+@dp.message_handler(text='Узнать остаток сетов на абонементе')
+async def cmd_start(message: types.Message):
     await Form.number_subscriptions.set()
-    await message.reply("Введите номер абонемента:")
+    await message.reply("Введите номер абонемента:", reply_markup=kb.button_markup)
 
 
 @dp.message_handler(state=Form.number_subscriptions)
@@ -59,6 +76,21 @@ async def get_number_subscription(message: types.Message, state: FSMContext):
                 # reply_markup=markup,
                 parse_mode=ParseMode.MARKDOWN,
             )
+
+
+@dp.message_handler(text='Instagram')
+async def get_to_instagram(message: types.Message):
+    pass
+
+
+@dp.message_handler(text='Группа Вконтакте')
+async def get_to_public_vk(message: types.Message):
+    pass
+
+
+@dp.message_handler(text='Позвонить')
+async def call_to_admin(message: types.Message):
+    pass
 
 
 if __name__ == '__main__':
